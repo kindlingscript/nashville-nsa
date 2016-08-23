8601a5135728da8df46b6a70182d3d4f19b9d101
Rebecca Taylor <inflamedmuse@gmail.com>
2016-08-23 23:14:28 +0000

    Customize devise to add/persist user's real names in sign-up

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations' }
   root to: 'static_pages#index'
   # resources :users, only: :show
   get 'users/:username', to: 'users#show', as: :user
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160823065345) do
+ActiveRecord::Schema.define(version: 20160823223322) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,8 @@ ActiveRecord::Schema.define(version: 20160823065345) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "username"
+    t.string   "firstname"
+    t.string   "lastname"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
